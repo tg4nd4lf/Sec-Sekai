1699716b5001681f252c32f7e0ace3cc82ecac6a
klaus-moser <60796711+klaus-moser@users.noreply.github.com>
2023-12-28 01:58:07 +0100

    Complete test functions.

--- test/test_utils/test_airmon.py
+++ test/test_utils/test_airmon.py
@@ -29,9 +29,26 @@ class TestAirmon:
             mock_subprocess_call.assert_called_once_with(command="iwconfig")
             assert result == expected
 
-    def test_is_monitor_enabled(self):
-        # TODO
-        pass
+    @pytest.mark.parametrize("test_input, expected", [
+        (["", "ERROR", -1], None),
+        (["wlan0  IEEE 802.11  Mode:Managed", "", 0], None),
+        (["wlan0  IEEE 802.11  Mode:Monitor", "", 0], "wlan0"),
+        (["wlan0mon  IEEE 802.11  Mode:Monitor", "", 0], "wlan0mon")
+    ])
+    def test_is_monitor_enabled(self, test_input, expected) -> None:
+        """
+        Test the functino 'is_monitor_enabled'.
+        """
+
+        # arrange
+        with patch('src.utils.airmon.subprocess_call') as mock_subprocess_call:
+            # arrange
+            mock_subprocess_call.return_value = (test_input[0], test_input[1], test_input[2])
+            # act
+            result = Airmon.get_monitor_interface_name()
+            # assert
+            mock_subprocess_call.assert_called_once_with(command="iwconfig")
+            assert result == expected
 
     def test_switch_monitor_mode(self):
         # TODO
